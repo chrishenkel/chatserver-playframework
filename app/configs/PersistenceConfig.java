d6da57b30ba3ed82be6b358df91b05766f84c6ba
Chris Henkel <chrishenkel2@gmail.com>
2014-02-21 15:52:55 -0500

    test

--- app/configs/PersistenceConfig.java
+++ app/configs/PersistenceConfig.java
@@ -63,10 +63,6 @@ public class PersistenceConfig {
 		dataSource.setDriverClassName(driver);
 		dataSource.setUrl(Play.application().configuration()
 				.getString("db.default.url"));
-		dataSource.setUsername(Play.application().configuration()
-				.getString("db.default.user"));
-		dataSource.setPassword(Play.application().configuration()
-				.getString("db.default.password"));
 		return dataSource;
 	}
 
